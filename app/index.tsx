11952c8bfefe6991743fd2506a3be79717ec5be2
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2025-07-04 13:51:17 -0300

    🎉 Commit inicial

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="flex-1 justify-center items-center bg-white">
+			<Text className="font-bold text-blue-500 text-xl">
+				Editar app/index.tsx para editar esta tela.
+			</Text>
+		</View>
+	);
+}
